17cc8ce104ba7f35d1eba1e8ab746852338190c6
muhammadmehdi09 <120133578+muhammadmehdi09@users.noreply.github.com>
2024-02-13 11:26:32 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,26 +1,51 @@
-let email = document.getElementById('email')
-let password = document.getElementById('password')
-let username = document.getElementById('username')
-let login_email = document.getElementById('login_email')
-let login_password = document.getElementById('password_email')
-let login_div = document.getElementById('')
-let users = []
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+let username = document.getElementById("username");
+let login_username = document.getElementById("login_username");
+let login_password = document.getElementById("login_password");
+let msg = document.getElementById("login_password");
+let users = [];
 
 const signup = () => {
+    if (users.some((user) => user.email === email.value)) {
+        alert("Sorry Email is already registered");
+    }
+
+    else if (users.some((user) => user.username === username.value)) {
+        alert("Sorry username is already in use");
+    } 
+    
+    else if (username.value.length < 3) {
+        alert("Username must have at least 3 charachters");
+    }
+
+    else if (password.value.length < 5) {
+        alert("Password must have at least 5 charachters");
+    }
+
+    else {
         let user = {
             username: username.value,
             email: email.value,
-            password: password.value
-        }
-    
-        users.push(user)
-        console.log(users)
-}
+            password: password.value,
+        };
+        users.push(user);
+        alert(`Hello ${user.username}`);
+    }
+};
 
 const login = () => {
-     if(users.some(user => user.email === login_email.value)){
-        if(users.some(user => user.password === login_password.value)){
-            document.write("Hello")
+    if (users.some((user) => user.username === login_username.value)) {
+        if (users.some((user) => user.password === login_password.value)) {
+            alert(`Hello ${login_username.value}`);
         }
-     }
-}
+
+        else {
+            alert("Sorry password is wrong");
+        }
+    }
+
+    else {
+        alert("Sorry username is not registered");
+    }
+};
